44528aebfc7d5b15000eff851d5a27f76e1a3dc7
yagshi <yagshi@ugsf.info>
2019-02-14 12:18:12 +0000

    tiny bug fix

--- piirremcon.c
+++ piirremcon.c
@@ -276,7 +276,7 @@ int piir_initialize(int gpio_led, int pi_type)
 int hexval(unsigned char ch) {
   if (ch >= '0' && ch <= '9') return ch - '0';
   if (ch >= 'a' && ch <= 'f') return ch - 'a' + 10;
-  if (ch >= 'A' && ch <= 'F') return ch - 'a' + 10;
+  if (ch >= 'A' && ch <= 'F') return ch - 'A' + 10;
   return 0;
 }
 
